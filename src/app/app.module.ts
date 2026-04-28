a753d8f770ebc9082660af8496915500abcbeb31
AMAN-AKC <amanchauhan10122004@gmail.com>
2026-04-28 23:05:14 +0530

    inital setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { LoginComponent } from './components/login/login.component';
+import { RestpasswordComponent } from './components/restpassword/restpassword.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    LoginComponent,
+    RestpasswordComponent
   ],
   imports: [
     BrowserModule,
